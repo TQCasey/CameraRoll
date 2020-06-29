9051a81d5b076c4cfc53556611c0536103d6db19
TQ <494294315@qq.com>
2020-06-29 17:04:47 +0800

    web

---
+++ js/CameraRoll.web.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CameraRoll extends Component {
+    constructor (props) {
+        super (props);
+    }
+
+    render () {
+        return null;
+    }
+}
+
+export default CameraRoll;
